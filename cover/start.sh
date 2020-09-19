dbb74710ea5123d7afb6487e72f978ecef578e84
Archit Parnami <architparnami@gmail.com>
2020-09-19 19:20:39 -0400

    build autograding VMs for courses

--- cover/start.sh
+++ cover/start.sh
@@ -1,5 +1,14 @@
 service docker start;
-echo "starting docker...";
+echo "Waiting 30s for docker to start in Tango ...";
 sleep 30s;
-docker build -t autograding_image vmms/;
+
+#Uncomment the following line to build the default autograding image
+#docker build -t autograding_image vmms/;
+
+echo "Building autograding VM for ITCS-5152"
+docker build -t autograding_itcs_5152 vmms/ITCS-5152/;
+
+echo "Building autograding VM for ITCS-4156"
+docker build -t autograding_itcs_4156 vmms/ITCS-4156/;
+
 service supervisor start;
